3134809badeb3fd6525e3edea324c2e31ed4e018
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2022-11-11 18:23:23 +0100

    update name linguaggi chapters

--- sistemi_operativi/esercizi/es1/parallel_max.c
+++ sistemi_operativi/esercizi/es1/parallel_max.c
@@ -1,52 +1,78 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <limits.h>
 
-#define N 125
-#define M 4
+#define N 120
+#define K 10
 
-struct args {
-    int *a;
-    int start;
-    int length;
-  
-};
+int V[N];
 
-void *max(void *args) {
-    struct args *_args = (struct args *)args;
+void *max(void *t)
+{
+    int first = (int)t;
 
-    printf("Thread %lu: start: %d, length: %d\n", pthread_self(), _args->start, _args->length);
-
-    int max = _args->a[_args->start];
-    for (int i = _args->start+1; i < (_args->start + _args->length); i++) {
-        if (_args->a[i] > max) {
-            max = _args->a[i];
+    int max = V[first];
+    for (int i = 0; i < first + K; i++)
+    {
+        if (V[i] > max)
+        {
+            max = V[i];
         }
     }
     pthread_exit((void *)max);
 }
 
-int main() {
-    int V[N], K, split;
-    struct args *args = (struct args *)malloc(M * sizeof(struct args));
-    args->a = (int *)malloc(N * sizeof(int));
+int main()
+{
+    int M, max_value = INT_MIN;
+    M = N / K;
     pthread_t threads[M];
 
-    K = N/M;
-    printf("K = %d, split = %d\n", K, split);
-    for (int i = 0; i < N; i++) {
-        args->a[i] = rand() % 100;
+    srand(time(0));
+    for (int i = 0; i < N; i++)
+    {
+        V[i] = 1 + rand() % 100;
     }
-    args->start = 0;
-    args->length = K;
 
-    for (int i = 0; i < M; i++) {
-        printf("Start: %d, length: %d\n", args->start, args->length);
-        pthread_create(&threads[i], NULL, max, (void *)args);
-        args->start += K;
-        if (i == (M - 2)) {
-            args->length = K + split;
+    for (int i = 0; i < M; i++)
+    {
+        int rc = pthread_create(&threads[i], NULL, max, (void *)(i * K));
+        if (rc)
+        {
+            printf("Error creating thread %d with code %d", i, rc);
+            exit(-1);
         }
     }
-    free(args);
+
+    for (int i = 0; i < M; i++)
+    {
+        int status;
+        pthread_join(threads[i], (void *)&status);
+        // printf("Thread %lu finished with result %d", threads[i], status);
+
+        if (status > max_value)
+        {
+            max_value = status;
+        }
+    }
+
+    printf("Max value: %d", max_value);
+    int check_correct = (1 == 1), check_found = (1 == 0);
+    for (int i = 0; i < N; i++)
+    {
+        if (V[i] == max_value && !check_found)
+        {
+            printf(" at index %d\n", i);
+            check_found = (1 == 1);
+        }
+        if (V[i] > max_value)
+        {
+            check_correct = (1 == 2);
+        }
+    }
+    if (check_correct)
+    {
+        printf("The max value is correct\n");
+    }
 }
